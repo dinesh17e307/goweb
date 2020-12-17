95c3801d7ac3f0389724d6fabe38750ebe43ad3e
dinesh17e307 <dinisellappan@gmail.com>
2020-12-17 22:09:41 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
  import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,MatButtonModule],
+  imports:      [ BrowserModule, FormsModule,MatButtonModule,MatGridListModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
